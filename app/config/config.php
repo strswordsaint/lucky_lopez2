1425d7dd1f3101b5196628689d268766e10c2634
strswordsaint <lopezluckyrainsell@gmail.com>
2025-10-08 20:02:41 +0800

    first commit

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://lucky-lopez23.onrender.com';
+$config['base_url'] 				= 'https://lucky-lopez23.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
